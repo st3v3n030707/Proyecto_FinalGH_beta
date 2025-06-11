5368bb73d29f6cfbb4ea3018441267984ffb6b50
Naza2006 <nazario@uamv.edu.ni>
2025-06-11 16:34:22 -0600

    fin del dao clientes

--- dao/clientes_dao.py
+++ dao/clientes_dao.py
@@ -11,3 +11,12 @@ def buscar_cliente(id_cliente):
     for cliente in clientes:
         if cliente.id_cliente == id_cliente:
             return cliente
+    return None
+
+def eliminar_cliente(id_cliente):
+    global clientes
+    clientes = [c for c in clientes if c.id_cliente != id_cliente]
+    guardar_en_archivo("data/clientes.txt", clientes)
+
+def listar_clientes():
+    return clientes
