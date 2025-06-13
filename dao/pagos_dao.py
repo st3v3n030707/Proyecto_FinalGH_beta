39ea4b188ad4101ddce2b3448a4b59fe86e31253
st3v3n030707 <safloreso@uamv.edu.ni>
2025-06-13 16:52:13 -0600

    se agrego la parte de nazario

---
+++ dao/pagos_dao.py
@@ -0,0 +1,19 @@
+from modulo.pago import Pago
+from modulo.archivo import guardar_en_archivo, leer_desde_archivo
+
+pagos = leer_desde_archivo("data/pagos.txt", Pago)
+
+def registrar_pago(pago):
+    pagos.append(pago)
+    guardar_en_archivo("data/pagos.txt", pagos)
+    
+def listar_pagos():
+    return pagos
+
+def eliminar_pago(id_pago):
+    global pagos
+    pagos = [p for p in pagos if p.id_pago != id_pago]
+    guardar_en_archivo("data/pagos.txt", pagos)
+
+def pagos_por_prestamo(id_prestamo):
+    return [p for p in pagos if p.id_prestamo == id_prestamo]
